6d30cf8682a2e739098e850fbb0bd927d595b0b4
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-04 18:43:32 +0200

    Create home route.

---
+++ frontend/src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+
+import Home from './views/Home'
+
+export const routes = (
+  <Route path="/" component={Home}>
+  </Route>
+)
