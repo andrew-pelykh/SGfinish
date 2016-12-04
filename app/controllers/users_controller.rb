afffc5bb575d09e5d395de6fa586315beb0b8f90
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-04 21:04:54 +0200

    Create get_user action in users controller with relative route.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def home
   end
+
+  def get_user
+    @user = User.find(params[:id])
+    render json: @user.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'users#home'
-
+  get 'get_user/:id' => 'users#get_user'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
