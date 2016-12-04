bd1722b5d9bfd592f47a09d352a5bd62c79f1149
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-04 18:38:46 +0200

    Create index.js file.

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { browserHistory } from 'react-router'
+import Root from './Root'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
+
+render(
+  <Root store={store} history={browserHistory} />,
+  document.getElementById('app')
+)
