029f02d25d26f399de4de91030689bc144edff44
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 22:57:03 +0200

    Change mark=up of NavBar container.

--- frontend/src/containers/NavBar.js
+++ frontend/src/containers/NavBar.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-
+import { hashHistory } from 'react-router'
+import { signOut } from '../actions/UserActions'
 class NavBar extends Component {
 
   render() {
 
     return (
         <div>
-            <Link to='/signup'>SignUp</Link>
+            <a href='#'  onClick={(e) => this.onClick(e)}>SignOut</a>
             {this.props.children}
         </div>
-    );
+    )
+  }
+
+  onClick(event){
+    event.preventDefault()
+    const { dispatch } = this.props
+    dispatch(signOut())
+    hashHistory.push(`/signup`)
   }
 }
 
