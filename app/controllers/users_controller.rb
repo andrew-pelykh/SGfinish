a568d897f9b487d568e669508a7712827fc8a090
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 10:06:04 +0200

    Add create_user action to users controller with relative route.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+  
   def home
   end
 
@@ -6,4 +8,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: @user.to_json
   end
+
+  def create_user
+    @user = User.new user_params
+    if @user.save
+      render json: @user.to_json
+    else
+      render json: @user.to_json
+    end
+  end
+
+  private
+
+    def user_params
+       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#home'
   get 'get_user/:id' => 'users#get_user'
+  post 'create_user' => 'users#create_user'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
