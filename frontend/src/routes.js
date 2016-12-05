527486d8c445e32ede6e72dea8dabe45adcddfea
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 10:02:00 +0200

    Change routes. Add signup route.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
+import NavBar from './containers/NavBar'
 import Home from './views/Home'
+import SignUp from './views/SignUp'
 
 export const routes = (
-  <Route path="/" component={Home}>
+  <Route path="/" component={NavBar}>
+    <IndexRoute component={Home} />
+    <Route path="signup" component={SignUp} />
   </Route>
 )
