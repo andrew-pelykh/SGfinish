055a553210bceb59639a739702bedf54a3933aea
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 10:25:56 +0200

    Change browserHistory to hashHistory.

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
+import { Router, hashHistory } from 'react-router'
 import configureStore from './store/configureStore'
 import { routes } from './routes'
 
@@ -10,7 +10,7 @@ export default class Root extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router history={browserHistory} routes={routes} />
+        <Router history={hashHistory} routes={routes} />
       </Provider>
     )
   }
