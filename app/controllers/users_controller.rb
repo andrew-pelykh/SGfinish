98f8f79d4f711e9a98f853655efb01b8b03408fc
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 20:37:55 +0200

    Add user id to response json in users and sessions controller. Add get_current_user action to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,20 @@ class UsersController < ApplicationController
 
   def get_user
     user = User.find(params[:id])
-    render json: { name:user.name, email:user.email }.to_json
+    render json: { name:user.name, email:user.email, id:user.id }.to_json
   end
 
   def create_user
     user = User.new user_params
     if user.save
-      render json: { name:user.name, email:user.email }.to_json
+      render json: { name:user.name, email:user.email, id:user.id }.to_json
     else
-      render json: { name:user.name, email:user.email }.to_json
+      render json: { name:user.name, email:user.email, id:user.id }.to_json
     end
+  end
+
+  def get_current_user
+    render json: { name:current_user.name, email:current_user.email, id:current_user.id }.to_json
   end
 
   private
