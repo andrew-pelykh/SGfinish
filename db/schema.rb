dfae14ee29ffc9aeb27b5d65a1baf3d67af2c32b
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 19:46:12 +0200

    Create and run AddRememberTokenToUsers migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204192323) do
+ActiveRecord::Schema.define(version: 20161205174505) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.string "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
