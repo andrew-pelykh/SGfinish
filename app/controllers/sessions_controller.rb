85e72ce0b4aad57f997b9d7caf1951d02f41dd71
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 19:46:45 +0200

    Create sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+
+  def signin_user
+    user = User.find_by(email: params[:user][:email].downcase)
+    if user && user.authenticate(params[:user][:password])
+      sign_in user
+      render json: user.to_json
+    else
+      render json: user.to_json
+    end
+  end
+
+  def destroy
+    signout
+    redirect_to root_url
+  end
+
+end
