7e6c08d5d546c7130f869372cb1c2a030a330b7d
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-05 10:02:44 +0200

    Create SignUp view.

---
+++ frontend/src/views/SignUp/index.js
@@ -0,0 +1,30 @@
+import React, { Component} from 'react'
+import { connect } from 'react-redux'
+import { signUp } from '../../actions/UserActions'
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Sign on</h2>
+         <form onSubmit={(e) => this.onSubmitSignOn(e)}>
+           <p><input type="text" id="name" placeholder="Name" /></p>
+           <p><input type="text" id="email" placeholder="Email" /></p>
+           <p><input type="text" id="password" placeholder="Password" /></p>
+           <p><input type="text" id="password_confirmation" placeholder="Password confirmation" /></p>
+           <p><button>Create</button></p>
+         </form>
+     </div>
+    )
+  }
+
+  onSubmitSignOn(event){
+    event.preventDefault()
+    const { dispatch } = this.props
+    dispatch(signUp())
+  }
+}
+
+
+
+export default connect()(SignUp)
