dee243a10e06926aee6bcc2467e35ec1a12882db
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-06 20:59:59 +0200

    Add update_user route to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post 'signin_user' => 'sessions#signin_user'
   delete 'signout_user' => 'sessions#signout_user'
   get 'get_current_user' => 'users#get_current_user'
+  patch "/update_user" => 'users#update_user'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
