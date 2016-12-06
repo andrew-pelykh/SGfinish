34579b8e0650679aae2bd9267d234cb94a089a0d
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-06 23:15:57 +0200

    Add get_posts function to posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,8 @@
 class PostsController < ApplicationController
   before_filter :find_post,      only: [:edit, :show, :update, :destroy]
+
+  def get_posts
+    user = User.find(params[:id])
+    render json: user.posts.all
+  end
 end
