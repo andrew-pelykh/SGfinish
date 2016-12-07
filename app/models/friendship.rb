8813dc5289dcefcf832042205d64590a3d5f1639
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-07 17:50:16 +0200

    Create friendship model. Add associations between users through friendships.

---
+++ app/models/friendship.rb
@@ -0,0 +1,7 @@
+class Friendship < ActiveRecord::Base
+
+  validates_presence_of :user_id, :friend_id
+
+  belongs_to :user
+  belongs_to :friend, :class_name => "User"
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
   has_many :posts, dependent: :destroy
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
 
   def User.new_remember_token
