9439ff7fc93d52dce11c2fcaf4c5875c228ac81c
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-07 17:47:08 +0200

    Create and run CreateFriendships migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206190135) do
+ActiveRecord::Schema.define(version: 20161207154613) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
