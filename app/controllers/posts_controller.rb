ec20ef1e78fea8d98196ca4f450ff7930d570295
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-07 17:12:46 +0200

    Add create_post action to posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
-  before_filter :find_post,      only: [:edit, :show, :update, :destroy]
+  skip_before_filter  :verify_authenticity_token
 
   def get_posts
     user = User.find(params[:id])
     render json: user.posts.all
+  end
+
+  def create_post
+    post = current_user.posts.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: post
+    end
+  end
+
+  def post_params
+     params.require(:post).permit(:title, :body)
   end
 end
