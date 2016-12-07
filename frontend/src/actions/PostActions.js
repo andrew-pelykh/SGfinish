ac7a554c743e7e115bc43883d44d9b82c447a1e4
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-07 17:09:35 +0200

    Add createPost function and action creaters.

--- frontend/src/actions/PostActions.js
+++ frontend/src/actions/PostActions.js
@@ -22,3 +22,34 @@ export function getPosts(id) {
       .then(response => { dispatch(postsSuccess(response.data)) })
   }
 }
+
+export const POST_SUCCESS = 'USER_SUCCESS'
+export const POST_CREATE = 'POST_CREATE'
+
+export function postSuccess(json) {
+  return {
+    type: POST_SUCCESS,
+    posts: json
+  }
+}
+
+export function postCreate() {
+  return {
+    type: POST_CREATE
+  }
+}
+
+export function createPost() {
+    let new_post = {}
+    new_post.title = document.getElementById('title').value
+    new_post.body = document.getElementById('body').value
+    return dispatch => {
+      dispatch(postCreate(new_post))
+    return axios.post('http://localhost:3000/create_post',
+                       {post: new_post})
+    .then(response => {
+      dispatch(postSuccess(response.data))
+      return response
+    })
+  }
+}
