dfad0b4a2c4852a485cbcf95af516209ba0f61e3
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-08 18:18:39 +0200

    Update get_friends action in friendships controller.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,7 @@ class FriendshipsController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def get_friends
-    if params[:limit] == 0
+    if params[:limit] == "0"
       render json: User.where(id: Friendship.where(user_id:params[:id]).map {|el| el=el.friend.id }).to_json
     else
       render json: User.where(id: Friendship.where(user_id:params[:id]).map {|el| el=el.friend.id }).limit(params[:limit]).to_json
