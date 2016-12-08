84b018445491be8aeea63dffd0e33735e27ca4f5
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-08 22:05:09 +0200

    Create and run AddPhotoToPosts migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208175256) do
+ActiveRecord::Schema.define(version: 20161208200413) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161208175256) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "photo"
   end
 
   add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
