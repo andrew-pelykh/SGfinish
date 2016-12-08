a7ccd89ba8a63c9a20b4f1e485f924bf7a078280
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-08 16:47:43 +0200

    Add users list to Home view.

--- frontend/src/views/Home/index.js
+++ frontend/src/views/Home/index.js
@@ -3,11 +3,16 @@ import { connect } from 'react-redux'
 import { getCurrentUser, getFriends } from '../../actions/UserActions'
 import { getPosts } from '../../actions/PostActions'
 import PostsList from '../../components/Posts/PostsList'
+import UsersList from '../../components/Users/UsersList'
 
 class Home extends Component {
   componentDidMount() {
     const { dispatch } = this.props
     dispatch(getCurrentUser())
+    if (this.props.current_user.name !== ""){
+      dispatch(getPosts(this.props.current_user.id))
+      dispatch(getFriends(this.props.current_user.id,8))
+    }
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.current_user.id !== this.props.current_user.id){
@@ -24,6 +29,7 @@ class Home extends Component {
       <div>
         <p>{this.props.current_user.name}</p>
         <PostsList posts={Object.keys(this.props.posts).map(key => this.props.posts[key])} />
+        <UsersList users={Object.keys(this.props.users).map(key => this.props.users[key])} />
       </div>
     )
   }
@@ -32,7 +38,8 @@ class Home extends Component {
 function mapStateToProps(state) {
   return {
     current_user: state.current_user,
-    posts: state.posts
+    posts: state.posts,
+    users: state.users
   }
 }
 
