2a0bcc34f956e8f3d90c9a28ea06eafa7f46cb44
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-08 18:15:09 +0200

    Create Friends container.

---
+++ frontend/src/containers/Friends.js
@@ -0,0 +1,34 @@
+import React, { Component} from 'react'
+import { connect } from 'react-redux'
+import { getFriends } from '../actions/UserActions'
+import UsersList from '../components/Users/UsersList'
+
+class Friends extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+    dispatch(getFriends(this.props.params.id))
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.params.id !== this.props.params.id) {
+      const { dispatch } = this.props
+      dispatch(getFriends(nextProps.params.id))
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>All friends</h1>
+        <UsersList users={Object.keys(this.props.users).map(key => this.props.users[key])} />
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    users: state.users
+  }
+}
+
+export default connect(mapStateToProps)(Friends)
