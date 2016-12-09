49dd5138c9c6af3bfa73614db376e5527cc4ac7d
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-09 16:02:32 +0200

    Create photos controller. Add get_photos actions.

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,12 @@
+class PhotosController < ApplicationController
+
+  def get_photos
+    if params[:limit] == 0
+      photos = User.find(params[:id]).photos
+      render json: photos.to_json
+    else
+      photos = User.find(params[:id]).photos.limit(params[:limit])
+      render json: photos.to_json
+    end
+  end
+end
