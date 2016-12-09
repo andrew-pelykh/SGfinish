0bfa68ebd75748e5d6dd382442a1ec1371f972db
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-09 16:01:16 +0200

    Create PhotosList component.

---
+++ frontend/src/components/Photos/PhotosList.js
@@ -0,0 +1,19 @@
+import React, { Component} from 'react'
+import { Link } from 'react-router'
+
+export default class PhotosList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.photos.map(photo => {
+          return (
+              <div key={'photo'+photo.id}>
+                <img src={photo.url}/>
+              </div>
+          )
+        })
+       }
+    </div>
+     )
+  }
+}
