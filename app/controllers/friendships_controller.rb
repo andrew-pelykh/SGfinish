1151e0edfdc86a63002eadcf0f86c7162cb8aa5b
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-09 22:20:31 +0200

    Refactor friendships controller.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,29 +3,29 @@ class FriendshipsController < ApplicationController
 
   def get_friends
     if params[:limit] == "0"
-      users = map_users(User.where(id: Friendship.where(user_id:params[:id]).map {|el| el=el.friend.id }))
-      render json: users.to_json
+      friends_id = Friendship.where(user_id:params[:id]).map {|el| el=el.friend.id }
+      users = map_users(User.where(id: friends_id))
     else
-      users = map_users(User.where(id: Friendship.where(user_id:params[:id]).map {|el| el=el.friend.id }).limit(params[:limit]))
-      render json: users.to_json
+      users = map_users(User.where(id: friends_id).limit(params[:limit]))
     end
+    render json: users.to_json
   end
 
   def add_friend
-    friendship = Friendship.new(user_id:current_user.id, friend_id: params[:id])
+    friendship = Friendship.create(user_id:current_user.id, friend_id: params[:id])
     user = User.find(params[:id])
-    if friendship.save
-      render json: { name:user.name, email:user.email, id:user.id, avatar:user.avatar.url, isFriend: true }.to_json
-    else
-      render json: { name:user.name, email:user.email, id:user.id, avatar:user.avatar.url, isFriend:(Friendship.exists?(user_id:current_user.id, friend_id:user.id))}.to_json
-    end
+    render_friend(user, params[:id])
   end
 
   def delete_friend
     friendship = Friendship.find_by(user_id:current_user.id, friend_id: params[:id])
     user = User.find(params[:id])
     friendship.destroy
-      render json: { name:user.name, email:user.email, id:user.id, avatar:user.avatar.url, isFriend:(Friendship.exists?(user_id:current_user.id, friend_id:user.id))}.to_json
+    render_friend(user, params[:id])
   end
 
+  def render_friend(user, friend_id)
+    render json: { name:user.name, email:user.email, id:user.id, avatar:user.avatar.url,
+      isFriend:(Friendship.exists?(user_id:current_user.id, friend_id:user.id))}.to_json
+  end
 end
