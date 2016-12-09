55ba13f688835707c4dd5d522815dbfb718a402d
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-09 15:27:32 +0200

    Dirty commit.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 class AvatarUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
