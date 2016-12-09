a16f9648bb835feb3e4c32b46630d945ac1d9f4d
Andrew Pelykh <andrew.pelykh@gmail.com>
2016-12-09 19:26:26 +0200

    Improve factories.rb.

--- test/factories.rb
+++ test/factories.rb
@@ -5,4 +5,21 @@ FactoryGirl.define do
     password "password"
     password_confirmation "password"
   end
+
+  factory :post do
+    sequence(:title)  { |n| "title #{n}" }
+    sequence(:body) { "a"*10 }
+    user
+  end
+
+  factory :friendship do
+    sequence(:friend_id) { |n| n}
+    user
+  end
+
+  factory :photo do
+    sequence(:url) { "Some photo url" }
+    user
+  end
+
 end
